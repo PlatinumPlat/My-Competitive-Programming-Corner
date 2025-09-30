72f10af2ce9bf06ff5c880d8facf6c82901ed4ca
PlatinumPlat <twinsophiapu@gmail.com>
2025-09-29 20:54:18 -0400

    navigation
    side-by-side navigation
    added arrows
    more styles

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('arrow-button').forEach(btn => {
+    btn.addEventListener('click', e => {
+        e.preventDefault;
+        const traget = document.querySelector(btn.getAttribute('href'));
+        traget.scrollIntoView({ behaviour: 'smooth' });
+    });
+});
+
+const wrapper = document.querySelector('.sectionWrapper');
+let currentIndex = 0;
+const sections = document.querySelectorAll('section');
+const totalSections = sections.length;
+
+document.querySelectorAll('.arrow-button.right').forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (currentIndex < totalSections - 1) {
+            currentIndex++;
+            wrapper.style.transform = `translateX(-${currentIndex * 100}vw)`;
+        }
+    });
+});
+
+document.querySelectorAll('.arrow-button.left').forEach(btn => {
+    btn.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            wrapper.style.transform = `translateX(-${currentIndex * 100}vw)`;
+        }
+    });
+});
